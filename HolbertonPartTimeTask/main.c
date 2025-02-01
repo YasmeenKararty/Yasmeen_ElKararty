f86c21a73165901086e90e08cd8e2c427d0385cd
Yasmeen ElKararty <66736704+YasmeenKararty@users.noreply.github.com>
2025-02-01 22:39:16 +0200

    correcting comments

--- HolbertonPartTimeTask/main.c
+++ HolbertonPartTimeTask/main.c
@@ -7,9 +7,9 @@ int main()
     int numOfCharsPrinted = _printf("Name: %s\nFirst Letter: %c\n%%\nTask 1 done!\n----------\n","Yasmeen",'Y');
     // testing task 2
     _printf("Number of chars Printed by s: %i\nNumber of chars Printed by i in negative: %i\nTask 2 done!\n----------\n",numOfCharsPrinted,-numOfCharsPrinted);
-    // testing task 3
-    _printf("Binary Num of 98 is : %b\nTask 3 done!\n----------\n",98);
     // testing task 4
+    _printf("Binary Num of 98 is : %b\nTask 3 done!\n----------\n",98);
+    // testing task 5
     _printf("Unsigned int of 98: %u\nOctal representation of 98: %o\nHexdecimal representation of 98 using x: %x\nHexdecimal representation of 98 using X: %X\nBinary representation of 98 is : %b\nTask 4 done!\n----------\n",98,98,98,98,98);
 
 
